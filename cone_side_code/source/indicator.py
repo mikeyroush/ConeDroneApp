2b7e86508670c88c4e1459403cdfad0c0795934a
Myra L <h38383838@tamu.edu>
2020-11-10 21:14:22 -0600

    Update indication
    Indication is now able to be stopped at any time, rather than running for a pre-set amount of time.
    
    Additionally, indication has been combined into a single function, rather than one for LEDs and one for the flag.
    
    Next up is better integrating this into the wider code (node.py and all), and adding better commenting. Also, adding something to turn on LEDs without the flag (for startup lights, and such.)

--- cone_side_code/source/indicator.py
+++ cone_side_code/source/indicator.py
@@ -10,50 +10,70 @@ numPixels = 30
 pixels = neopixel.NeoPixel(board.D21, numPixels) #Connected to GPIO 21
 SERVO_PWM = 12 
 
+#setup, initialization
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO_PWM, GPIO.OUT)
+indicate_lock = threading.Lock()
+indicate_lock.acquire()
 
 def clearLEDS():
     pixels.fill((0, 0, 0))
     pass
+    
+def lowerFlag():
+    p = GPIO.PWM(SERVO_PWM, 50)
+    p.start(12)
+    time.sleep(.01)
 
 '''
 Displays a revolving pair of lights that circle around the LED strip
 '''
-def indicator_led(indicate, startup):
+def indicator_thread(startup, lock):
+    p = GPIO.PWM(SERVO_PWM, 50)
+    print("Starting indicator thread")
     pixel1 = 0
     pixel2 = 1
-    i = 0
-    desiredRotations = 2
-    if (indicate):
-        while(i < (numPixels * desiredRotations - 1) ):
-            clearLEDS()
-            if (startup):
-                pixels[pixel1] = (0, 0, 128)
-                pixels[pixel2] = (0, 0, 128)
-            else:
-                pixels[pixel1] = (0, 128, 0)
-                pixels[pixel2] = (0, 128, 0)
-            pixel1 += 1
-            pixel2 += 1
-            i +=1
-
-            if(pixel1 > (numPixels-1)):
-                pixel1 = 0
-            if(pixel2 > (numPixels-1)):
-                pixel2 = 0
-
-            time.sleep(0.1)
-
-    else:
-        clearLEDS()
-
-
-def indicator_flag(indicate):
-    p = GPIO.PWM(SERVO_PWM, 50) #pin 12, 50 Hz (20ms period)
-    if(indicate == True):
-        p.start(7) #7% duty cycle, go to 90 degrees
-    else:
-        p.start(12) #12% duty cycle, go back to 180 degrees. 
-    time.sleep(.02)
+    
+    #put the flag up
+    p.start(7)
+    time.sleep(0.05)
     p.stop()
+    
+    #start and maintain the LED pattern until our lock is locked.
+    while(not lock.locked() ):
+        clearLEDS()
+        if (startup):
+            pixels[pixel1] = (0, 0, 128)
+            pixels[pixel2] = (0, 0, 128)
+        else:
+            pixels[pixel1] = (0, 128, 0)
+            pixels[pixel2] = (0, 128, 0)
+        pixel1 += 1
+        pixel2 += 1
+
+        if(pixel1 > (numPixels-1)):
+            pixel1 = 0
+        if(pixel2 > (numPixels-1)):
+            pixel2 = 0
+
+        time.sleep(0.1)
+    print("indicator thread closing")
+    return
+    
+
+def indicator_start(startup):
+    indicate_lock.release()
+    inner_thread = threading.Thread(target=indicator_thread, args=(startup, indicate_lock))
+    inner_thread.start()
+    
+#stop the inner_thread loop, lower the flag, and clear the LEDs. 
+def indicator_stop():
+    print("stopping the indicatore")
+    indicate_lock.acquire()
+    #inner_thread.join() #This seems to yield "can't join before it's started"?
+    p = GPIO.PWM(SERVO_PWM, 50)
+    p.start(12) #12% duty cycle, go back to 180 degrees. 
+    p.ChangeDutyCycle(12)
+    time.sleep(.01)
+    p.stop()
+    clearLEDS()
